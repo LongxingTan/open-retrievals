978713188cf39e426cdcc4e50e981d703e6f5a4a
LongxingTan <tanlongxing888@163.com>
2024-05-09 15:16:04 +0800

    fix: update test example

--- src/retrievals/__init__.py
+++ src/retrievals/__init__.py
@@ -1,10 +1,6 @@
 from .data.collator import PairCollator, RerankCollator, TripletCollator
 from .data.dataset import RerankDataset, RetrievalDataset
-from .models.embedding_auto import (
-    AutoModelForEmbedding,
-    ListwiseModel,
-    PairwiseModel,
-)
+from .models.embedding_auto import AutoModelForEmbedding, ListwiseModel, PairwiseModel
 from .models.pooling import AutoPooling
 from .models.rerank import RerankModel
 from .models.retrieval_auto import AutoModelForRetrieval
