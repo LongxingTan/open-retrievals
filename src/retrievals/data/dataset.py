09d75b7ea43476009de51839d9a90d09f2b44fdb
LongxingTan <tanlongxing888@163.com>
2024-06-03 11:46:26 +0800

    fix: pipeline

--- src/retrievals/data/dataset.py
+++ src/retrievals/data/dataset.py
@@ -12,15 +12,31 @@ logger = logging.getLogger(__name__)
 
 
 class RetrievalDataset(Dataset):
-    def __init__(self, data_name_or_path: Union[str, datasets.Dataset], cache_dir: Optional[str] = None):
-        if os.path.isdir(data_name_or_path):
+    def __init__(
+        self,
+        data_name_or_path: Union[str, datasets.Dataset, None] = None,
+        train_group_size: int = 2,
+        query_key: str = 'query',
+        positive_key: str = 'positive',
+        negative_key='negative',
+        args: Optional = None,
+        tokenizer: PreTrainedTokenizer = None,
+    ):
+        if not data_name_or_path and args:
+            data_name_or_path = args.train_data
+        if args and 'train_group_size' in args:
+            self.train_group_size = args.train_group_size
+        else:
+            self.train_group_size = train_group_size
+
+        if isinstance(data_name_or_path, datasets.Dataset):
+            self.dataset = data_name_or_path
+        elif os.path.isdir(data_name_or_path):
             train_datasets = []
             for file in os.listdir(data_name_or_path):
                 temp_dataset = datasets.load_dataset(
                     "json",
                     data_files=os.path.join(data_name_or_path, file),
-                    split="train",
-                    cache_dir=cache_dir,
                 )
 
                 train_datasets.append(temp_dataset)
@@ -31,23 +47,33 @@ class RetrievalDataset(Dataset):
         if 'train' in self.dataset:
             self.dataset = self.dataset['train']
 
-        # self.tokenizer = tokenizer
+        self.tokenizer = tokenizer
+        self.args = args
+        self.query_key = query_key
+        self.positive_key = positive_key
+        self.negative_key = negative_key
         logger.info("Loaded {} retrieval data.".format(len(self.dataset)))
 
     def __len__(self) -> int:
         return len(self.dataset)
 
     def __getitem__(self, item: int) -> Union[Dict[str, str], List[BatchEncoding]]:
-        query = self.dataset[item]["query"]
-        if isinstance(self.dataset[item]["pos"], Iterable):
-            pos = random.choice(self.dataset[item]["pos"])
+        query = self.dataset[item][self.query_key]
+        if self.args and self.args.query_instruction is not None:
+            query = self.args.query_instruction + query
+
+        if isinstance(self.dataset[item][self.positive_key], Iterable):
+            pos = random.choice(self.dataset[item][self.positive_key])
         else:
-            pos = self.dataset[item]["pos"]
-        if isinstance(self.dataset[item]["neg"], Iterable):
-            neg = random.choice(self.dataset[item]["neg"])
-        else:
-            neg = self.dataset[item]["neg"][0]
-        sample = {"query": query, "pos": pos, "neg": neg}
+            pos = self.dataset[item][self.positive_key]
+
+        sample = {self.query_key: query, self.positive_key: pos}
+        if self.negative_key in self.dataset[item]:
+            if isinstance(self.dataset[item][self.negative_key], Iterable):
+                neg = random.choice(self.dataset[item][self.negative_key])
+            else:
+                neg = self.dataset[item][self.negative_key][0]
+            sample.update({self.negative_key: neg})
         return sample
 
     def dynamic_sample(self, batch_size: int, missing_list=None, wrong_dict=None, max_wrong: int = 16):
---
+++ src/retrievals/pipelines/finetune_embed.py
@@ -0,0 +1,177 @@
+import logging
+import os
+from dataclasses import dataclass, field
+from pathlib import Path
+from typing import Optional
+
+from transformers import (
+    AutoConfig,
+    AutoTokenizer,
+    HfArgumentParser,
+    TrainingArguments,
+    set_seed,
+)
+
+from ..data import PairCollator, RetrievalDataset, TripletCollator
+from ..models.embedding_auto import AutoModelForEmbedding
+from ..trainer import RetrievalTrainer
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class ModelArguments:
+    """
+    Arguments pertaining to which model/config/tokenizer we are going to fine-tune from.
+    """
+
+    model_name_or_path: str = field(
+        metadata={"help": "Path to pretrained model or model identifier from huggingface.co/models"}
+    )
+    config_name: Optional[str] = field(
+        default=None, metadata={"help": "Pretrained config name or path if not the same as model_name"}
+    )
+    tokenizer_name: Optional[str] = field(
+        default=None, metadata={"help": "Pretrained tokenizer name or path if not the same as model_name"}
+    )
+    cache_dir: Optional[str] = field(
+        default=None, metadata={"help": "Where do you want to store the pretrained models downloaded from s3"}
+    )
+
+
+@dataclass
+class DataArguments:
+    train_data: str = field(default=None, metadata={"help": "Path to train data"})
+    train_group_size: int = field(default=8)
+
+    query_max_len: int = field(
+        default=32,
+        metadata={
+            "help": "The maximum total input sequence length after tokenization for passage. Sequences longer "
+            "than this will be truncated, sequences shorter will be padded."
+        },
+    )
+
+    passage_max_len: int = field(
+        default=128,
+        metadata={
+            "help": "The maximum total input sequence length after tokenization for passage. Sequences longer "
+            "than this will be truncated, sequences shorter will be padded."
+        },
+    )
+
+    max_example_num_per_dataset: int = field(
+        default=100000000, metadata={"help": "the max number of examples for each dataset"}
+    )
+
+    query_instruction_for_retrieval: str = field(default=None, metadata={"help": "instruction for query"})
+    passage_instruction_for_retrieval: str = field(default=None, metadata={"help": "instruction for passage"})
+
+    def __post_init__(self):
+        if not os.path.exists(self.train_data):
+            raise FileNotFoundError(f"cannot find file: {self.train_data}, please set a true path")
+
+
+@dataclass
+class RetrieverTrainingArguments(TrainingArguments):
+    negatives_cross_device: bool = field(default=False, metadata={"help": "share negatives across devices"})
+    temperature: Optional[float] = field(default=0.02)
+    fix_position_embedding: bool = field(
+        default=False, metadata={"help": "Freeze the parameters of position embeddings"}
+    )
+    sentence_pooling_method: str = field(default='cls', metadata={"help": "the pooling method, should be cls or mean"})
+    normalized: bool = field(default=True)
+    use_inbatch_neg: bool = field(default=True, metadata={"help": "use passages in the same batch as negatives"})
+    train_type: str = field(default='pairwise', metadata={'help': "train type of point, pair, or list"})
+
+
+def main():
+    parser = HfArgumentParser((ModelArguments, DataArguments, TrainingArguments))
+    model_args, data_args, training_args = parser.parse_args_into_dataclasses()
+    model_args: ModelArguments
+    data_args: DataArguments
+    training_args: TrainingArguments
+
+    if (
+        os.path.exists(training_args.output_dir)
+        and os.listdir(training_args.output_dir)
+        and training_args.do_train
+        and not training_args.overwrite_output_dir
+    ):
+        raise ValueError(
+            f"Output directory ({training_args.output_dir}) already exists and is not empty. "
+            "Use --overwrite_output_dir to overcome."
+        )
+
+    # Setup logging
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s",
+        datefmt="%m/%d/%Y %H:%M:%S",
+        level=logging.INFO if training_args.local_rank in [-1, 0] else logging.WARN,
+    )
+    logger.warning(
+        "Process rank: %s, device: %s, n_gpu: %s, distributed training: %s, 16-bits training: %s",
+        training_args.local_rank,
+        training_args.device,
+        training_args.n_gpu,
+        bool(training_args.local_rank != -1),
+        training_args.fp16,
+    )
+    logger.info("Training/evaluation parameters %s", training_args)
+    logger.info("Model parameters %s", model_args)
+    logger.info("Data parameters %s", data_args)
+
+    # Set seed
+    set_seed(training_args.seed)
+
+    num_labels = 1
+    tokenizer = AutoTokenizer.from_pretrained(
+        model_args.tokenizer_name if model_args.tokenizer_name else model_args.model_name_or_path,
+        cache_dir=model_args.cache_dir,
+        use_fast=False,
+    )
+    config = AutoConfig.from_pretrained(
+        model_args.config_name if model_args.config_name else model_args.model_name_or_path,
+        num_labels=num_labels,
+        cache_dir=model_args.cache_dir,
+    )
+    logger.info('Config: %s', config)
+
+    model = AutoModelForEmbedding(
+        model_name=model_args.model_name_or_path,
+        normlized=training_args.normlized,
+        sentence_pooling_method=training_args.sentence_pooling_method,
+        negatives_cross_device=training_args.negatives_cross_device,
+        temperature=training_args.temperature,
+        use_inbatch_neg=training_args.use_inbatch_neg,
+    )
+
+    if training_args.fix_position_embedding:
+        for k, v in model.named_parameters():
+            if "position_embeddings" in k:
+                logging.info(f"Freeze the parameters for {k}")
+                v.requires_grad = False
+
+    train_dataset = RetrievalDataset(args=data_args, tokenizer=tokenizer)
+
+    trainer = RetrievalTrainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset,
+        data_collator=PairCollator(
+            tokenizer, query_max_len=data_args.query_max_len, passage_max_len=data_args.passage_max_len
+        ),
+        tokenizer=tokenizer,
+    )
+
+    Path(training_args.output_dir).mkdir(parents=True, exist_ok=True)
+
+    trainer.train()
+    trainer.save_model()
+
+    if trainer.is_world_process_zero():
+        tokenizer.save_pretrained(training_args.output_dir)
+
+
+if __name__ == "__main__":
+    main()
