cf6f2df78c218d920c57cab1d103160cccf5eff8
LongxingTan <tanlongxing888@163.com>
2024-06-21 09:57:21 +0800

    fix: embed

--- src/retrievals/models/embedding_auto.py
+++ src/retrievals/models/embedding_auto.py
@@ -339,26 +339,26 @@ class AutoModelForEmbedding(nn.Module):
         return all_embeddings
 
     def encode_queries(self, queries: List[str], **kwargs) -> np.ndarray:
-        '''
+        """For MTEB eval
         This function will be used for retrieval task
-        if there is a instruction for queries, we will add it to the query text
-        '''
+        if there is an instruction for queries, we will add it to the query text
+        """
         if self.query_instruction is not None:
             input_texts = ['{}{}'.format(self.query_instruction, q) for q in queries]
         else:
             input_texts = queries
-        return self.encode_from_text(input_texts)
+        return self.encode_from_text(input_texts, batch_size=4)
 
     def encode_corpus(self, corpus: List[Union[Dict[str, str], str]], **kwargs) -> np.ndarray:
-        '''
+        """For MTEB eval
         This function will be used for retrieval task
         encode corpus for retrieval task
-        '''
+        """
         if isinstance(corpus[0], dict):
             input_texts = ['{} {}'.format(doc.get('title', ''), doc['text']).strip() for doc in corpus]
         else:
             input_texts = corpus
-        return self.encode_from_text(input_texts)
+        return self.encode_from_text(input_texts, batch_size=4)
 
     def build_index(
         self,
