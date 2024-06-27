d743fe3e9e7a7ba9e24a1b9165aae89a38df59a4
LongxingTan <tanlongxing888@163.com>
2024-06-27 16:55:35 +0800

    tests: loss

--- tests/test_losses/test_infonce.py
+++ tests/test_losses/test_infonce.py
@@ -12,7 +12,7 @@ class InfoNCETest(TestCase, LossTesterMixin):
     def setUp(self) -> None:
         self.query_embedding = torch.rand(2, 11)
         self.positive_embedding = torch.rand(2, 11)
-        self.negative_embedding = torch.rand(2, 11)
+        self.negative_embedding = torch.rand(4, 11)
 
     def test_infonce_pair(self):
         loss_fn = InfoNCE(criterion=nn.CrossEntropyLoss())
@@ -29,5 +29,9 @@ class InfoNCETest(TestCase, LossTesterMixin):
         )
         self.assertEqual(paired_triplet_loss.shape, torch.Size([]))
         loss_fn = InfoNCE(criterion=nn.CrossEntropyLoss(), negative_mode='unpaired')
-        unpaired_triplet_loss = loss_fn(self.query_embedding, self.positive_embedding, self.negative_embedding)
-        self.assertEqual(unpaired_triplet_loss.shape, torch.Size([]))
+        triplet_loss_inbatch = loss_fn(self.query_embedding, self.positive_embedding, self.negative_embedding)
+        self.assertEqual(triplet_loss_inbatch.shape, torch.Size([]))
+
+        loss_fn = InfoNCE(use_inbatch_negative=False)
+        triplet_loss_wobatch = loss_fn(self.query_embedding, self.positive_embedding, self.negative_embedding)
+        self.assertEqual(triplet_loss_wobatch.shape, torch.Size([]))
