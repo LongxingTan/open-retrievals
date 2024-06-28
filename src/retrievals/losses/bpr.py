a0f6a82f3af86417842bfbcbd0978e8a867856f7
LongxingTan <tanlongxing888@163.com>
2024-06-28 21:35:45 +0800

    docs: readme

--- src/retrievals/losses/bpr.py
+++ src/retrievals/losses/bpr.py
@@ -3,8 +3,14 @@ import torch.nn as nn
 
 
 class BPRLoss(nn.Module):
+    """Bayesian Personalized Ranking"""
+
     def __init__(self):
         super(BPRLoss, self).__init__()
 
-    def forward(self):
-        return
+    def forward(
+        self,
+        query_embeddings: torch.Tensor,
+        positive_embeddings: torch.Tensor,
+    ):
+        return -(query_embeddings - positive_embeddings).sigmoid().log().sum()
