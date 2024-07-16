83b785310b2e747a15c5800f5fe4fa757f8e431c
LongxingTan <tanlongxing888@163.com>
2024-07-16 11:50:29 +0800

    tests: metrics

---
+++ tests/test_metrics/test_fbeta.py
@@ -0,0 +1,32 @@
+import unittest
+
+from src.retrievals.metrics.fbeta import get_recall
+
+
+class TestGetRecall(unittest.TestCase):
+    def test_get_recall(self):
+        qid2positive = {'q1': ['d1', 'd3'], 'q2': ['d1'], 'q3': ['d2', 'd3']}
+
+        qid2ranking = {'q1': ['d1', 'd2', 'd3', 'd4'], 'q2': ['d2', 'd1', 'd3'], 'q3': ['d1', 'd3', 'd2']}
+
+        cutoff_ranks = [2, 3]
+
+        expected_recall_at_2 = {
+            'q1': 1.0 / 2,  # Only d1 is in top 2
+            'q2': 1.0 / 1,  # d1 is in top 2
+            'q3': 0.0,  # Neither d2 nor d3 are in top 2
+        }
+
+        expected_recall_at_3 = {
+            'q1': 2.0 / 2,  # d1 and d3 are in top 3
+            'q2': 1.0 / 1,  # d1 is in top 3
+            'q3': 1.0 / 2,  # d3 is in top 3
+        }
+
+        expected_mean_recall_at_2 = sum(expected_recall_at_2.values()) / len(expected_recall_at_2)
+        expected_mean_recall_at_3 = sum(expected_recall_at_3.values()) / len(expected_recall_at_3)
+
+        result = get_recall(qid2positive, qid2ranking, cutoff_ranks)
+
+        self.assertAlmostEqual(result['recall@2'], expected_mean_recall_at_2, places=4)
+        self.assertAlmostEqual(result['recall@3'], expected_mean_recall_at_3, places=4)
---
+++ tests/test_metrics/test_mrr.py
@@ -0,0 +1,19 @@
+import unittest
+
+from src.retrievals.metrics.mrr import get_mrr
+
+
+class TestGetMRR(unittest.TestCase):
+    def test_get_mrr(self):
+        qid2positive = {'q1': ['d1', 'd3'], 'q2': ['d1'], 'q3': ['d2', 'd3']}
+        qid2ranking = {'q1': ['d1', 'd2', 'd3', 'd4'], 'q2': ['d2', 'd1', 'd3'], 'q3': ['d1', 'd3', 'd2']}
+        cutoff_rank = 3
+        expected_mrr = {
+            'q1': 1.0 / 1,
+            'q2': 1.0 / 2,
+            'q3': 1.0 / 2,
+        }
+
+        expected_mean_mrr = sum(expected_mrr.values()) / len(expected_mrr)
+        result = get_mrr(qid2positive, qid2ranking, cutoff_rank)
+        self.assertAlmostEqual(result[f'mrr@{cutoff_rank}'], expected_mean_mrr, places=4)
