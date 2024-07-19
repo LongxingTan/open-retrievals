e1f255c3a60c8fd22d9acd03dbacaf6b157a462c
LongxingTan <tanlongxing888@163.com>
2024-07-19 10:13:15 +0800

    fix: embed

--- src/retrievals/pipelines/embed.py
+++ src/retrievals/pipelines/embed.py
@@ -68,7 +68,7 @@ class DataArguments:
     positive_key: str = field(default='positive')
     negative_key: str = field(default='negative')
     is_query: bool = field(default=False)
-    encode_save_file: str = field(default='embed.pkl')
+    encoding_save_file: str = field(default='embed.pkl')
 
     def __post_init__(self):
         if self.data_name_or_path is not None:
@@ -157,13 +157,14 @@ def main():
     else:
         quantization_config = None
 
-    model = AutoModelForEmbedding.from_pretrained(
-        model_name_or_path=model_args.model_name_or_path,
-        pooling_method=training_args.pooling_method,
-        use_lora=training_args.use_lora,
-        quantization_config=quantization_config,
-    )
     if training_args.do_train:
+        model = AutoModelForEmbedding.from_pretrained(
+            model_name_or_path=model_args.model_name_or_path,
+            pooling_method=training_args.pooling_method,
+            use_lora=training_args.use_lora,
+            quantization_config=quantization_config,
+        )
+
         if training_args.loss_fn == 'infonce':
             loss_fn = InfoNCE(
                 nn.CrossEntropyLoss(label_smoothing=0.0),
@@ -213,6 +214,14 @@ def main():
             tokenizer.save_pretrained(training_args.output_dir)
 
     if training_args.do_encode:
+        model = AutoModelForEmbedding.from_pretrained(
+            model_name_or_path=model_args.model_name_or_path,
+            pooling_method=training_args.pooling_method,
+            use_lora=training_args.use_lora,
+            quantization_config=quantization_config,
+            lora_path=model_args.output_dir,
+        )
+
         max_length = data_args.query_max_length if data_args.is_query else data_args.document_max_length
         logger.info(f'Encoding will be saved in {training_args.output_dir}')
 
@@ -239,7 +248,7 @@ def main():
         embeddings = model.encode(encode_loader)
         lookup_indices = list(range(len(encode_dataset)))
 
-        with open(os.path.join(training_args.output_dir, data_args.encode_save_file), 'wb') as f:
+        with open(data_args.encoding_save_file, 'wb') as f:
             pickle.dump((embeddings, lookup_indices), f)
 
 
