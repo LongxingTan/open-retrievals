84fe64c70ea57aba371e1322a5ac3184b71768f1
LongxingTan <tanlongxing888@163.com>
2024-09-06 15:27:04 +0800

    fix: test

--- src/retrievals/data/dataset.py
+++ src/retrievals/data/dataset.py
@@ -85,6 +85,8 @@ class RetrievalTrainDataset(Dataset):
         if self.dataset_split in dataset:  # train or dev
             dataset = dataset[dataset_split]
         dataset = dataset.filter(lambda x: len(x[self.positive_key]) > 0)
+        if self.negative_key in dataset:
+            dataset = dataset.filter(lambda x: len(x[self.negative_key]) > 0)
 
         self.unfold_each_positive = unfold_each_positive
         self.tokenizer = tokenizer
